e31ac2c858ea74165bd8ab3f5d40cbeb57796cb5
umer khan <khanmu2003@gmail.com>
2024-11-13 21:39:15 +0500

    umer khan commit to teach inam

--- src/main/java/com/example/sdaproj/AttendeeLogin.java
+++ src/main/java/com/example/sdaproj/AttendeeLogin.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class EventOrganizerLogin extends Application {
+public class AttendeeLogin extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("event-organizer-login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("attendee-login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Event Organizer login");
+        stage.setTitle("Attendee login");
         stage.setScene(scene);
         stage.show();
     }
