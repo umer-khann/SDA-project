8e20d3b8f3918889ea229fa71b6d9e8dd747a6dc
umer khan <khanmu2003@gmail.com>
2024-11-15 21:57:57 +0500

    new files added

--- src/main/java/com/example/sdaproj/AddNewVenue.java
+++ src/main/java/com/example/sdaproj/AddNewVenue.java
@@ -4,16 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AddNewVenue extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("add-new-venue.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
 
-        stage.setTitle("Admin login");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/sdaproj/AttendeeRegistration.java
+++ src/main/java/com/example/sdaproj/AttendeeRegistration.java
@@ -11,7 +11,7 @@ public class AttendeeRegistration extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("attendee-reg.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Attendee Registration");
         stage.setScene(scene);
         stage.show();
