dadf8fb5482f380a166ef7dd1091587ab481fde9
umer khan <khanmu2003@gmail.com>
2024-11-15 23:51:57 +0500

    new files added

--- src/main/java/com/example/sdaproj/ManageEvent.java
+++ src/main/java/com/example/sdaproj/ManageEvent.java
@@ -1,9 +1,24 @@
 package com.example.sdaproj;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class ManageEvent {
+import java.io.IOException;
 
-    public void applychanges(ActionEvent actionEvent) {
+public class ManageEvent extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("manage-event.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/com/example/sdaproj/ManageEventController.java
+++ src/main/java/com/example/sdaproj/ManageEventController.java
@@ -1,4 +1,9 @@
 package com.example.sdaproj;
 
+import javafx.event.ActionEvent;
+
 public class ManageEventController {
+
+    public void buttonpress(ActionEvent actionEvent) {
+    }
 }
