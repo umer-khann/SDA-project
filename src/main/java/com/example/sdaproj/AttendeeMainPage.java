78f0cebaaa605bd786da0f1f3a24122011ded096
Inam Ullah Shaikh <inamullahshaikh01@gmail.com>
2024-11-15 12:06:24 +0500

    Main page added

--- src/main/java/com/example/sdaproj/AttendeeMainPage.java
+++ src/main/java/com/example/sdaproj/AttendeeMainPage.java
@@ -6,21 +6,17 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.util.Objects;
-
 public class AttendeeMainPage extends Application {
     double x,y = 0;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Attendee-main-page.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource("Attendee-main-page.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         root.setOnMousePressed(event -> {
             x = event.getSceneX();
             y = event.getSceneY();
         });
-
 
         root.setOnMouseDragged(event -> {
             primaryStage.setX(event.getScreenX() - x);
