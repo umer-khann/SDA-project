8e11509d6f1a42fb4f1c29faeda25245f63c0621
SADIA <hamzaimran66628@gmail.com>
2024-11-15 22:03:49 +0500

    linked

--- src/main/java/com/example/sdaproj/AttendeeLoginController.java
+++ src/main/java/com/example/sdaproj/AttendeeLoginController.java
@@ -1,10 +1,18 @@
 package com.example.sdaproj;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AttendeeLoginController {
 
@@ -40,4 +48,22 @@ public class AttendeeLoginController {
                 loginmessagelabel.setText("Please input full details");
             }
         }
+    public void backButtonOnAction(javafx.event.ActionEvent e) throws IOException {
+        loadPage("home-page.fxml", e);
+    }
+    private void loadPage(String fxmlFile, ActionEvent event) throws IOException {
+        // Load the FXML file
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+        BorderPane root = fxmlLoader.load(); // Assuming the root node is a BorderPane
+
+        // Get the stage from the event source
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set the new scene
+        stage.setScene(new Scene(root));
+
+        // Show the stage
+        stage.show();
+    }
     }
