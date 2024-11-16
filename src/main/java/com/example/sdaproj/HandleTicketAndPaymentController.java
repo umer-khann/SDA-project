16f5c3a76df8b2c5284a710be4438262039c731c
umer khan <khanmu2003@gmail.com>
2024-11-16 14:46:14 +0500

    new files added

---
+++ src/main/java/com/example/sdaproj/HandleTicketAndPaymentController.java
@@ -0,0 +1,15 @@
+package com.example.sdaproj;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
+public class HandleTicketAndPaymentController {
+    public void handleButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void setCreditCardType(KeyEvent keyEvent) {
+    }
+
+    public void saveData(ActionEvent actionEvent) {
+    }
+}
