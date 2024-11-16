2d389abdaf74afe6ea2428ae2ab7ce9b9334b250
umer khan <khanmu2003@gmail.com>
2024-11-16 23:38:26 +0500

    new files added

--- src/main/java/com/example/sdaproj/AttendeeEventRegistrationController.java
+++ src/main/java/com/example/sdaproj/AttendeeEventRegistrationController.java
@@ -1,17 +1,25 @@
 package com.example.sdaproj;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class AttendeeEventRegistrationController extends Application {
+
+    @FXML
+    private Button applychanges;
+
     //check
     @Override
     public void start(Stage stage) throws IOException {
@@ -26,22 +34,29 @@ public class AttendeeEventRegistrationController extends Application {
     }
 
     public void buttonpress(ActionEvent e) throws IOException {
-        loadPage("Handle-ticket-and-payment.fxml",e);
+        AnchorPane parentPane = (AnchorPane) applychanges.getParent();
+        Stage stage = (Stage) applychanges.getScene().getWindow();
+        loadPage("Handle-ticket-and-payment.fxml", e, parentPane);
     }
 
-    private void loadPage(String fxmlFile, ActionEvent event) throws IOException {
-        // Load the FXML file
+    // Method to load a new FXML into the content area
+    private void loadPage(String fxml, ActionEvent e, AnchorPane contentArea) {
+        TranslateTransition slide = new TranslateTransition();
+        slide.setDuration(Duration.seconds(0.4));
+        slide.setToX(0);
+        slide.play();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
-        AnchorPane root = fxmlLoader.load(); // Assuming the root node is a BorderPane
-
-        // Get the stage from the event source
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-        // Set the new scene
-        stage.setScene(new Scene(root));
-
-        // Show the stage
-
+        contentArea.setVisible(true);
+        contentArea.setPrefWidth(730);
+        try {
+            // Load the FXML file
+            Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+            // Clear the current content and set the new content
+            contentArea.getChildren().clear(); // Clear any existing children
+            contentArea.getChildren().add(pane); // Add the new FXML content as a child
+        } catch (IOException e1) {
+            e1.printStackTrace(); // Log the error if loading fails
+        }
     }
 }
+
